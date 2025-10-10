aef8b66f63d603402c96cde2997c1f032f773699
Ekin Sipahi <ekinsipahi8@gmail.com>
2025-10-11 02:14:55 +0300

    Settings allowed hepsi ekleme

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-og-03q*s7tt+!f@*4f%!-th8&1#prrb3$ad*9vkc7_+an#j&j*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  
 
 
 # Application definition
